383f956e8c46f202490324826b95ddfc4cb1eb50
Satori32 <Huga8989+githubf@gmail.com>
2021-10-10 11:12:21 +0900

    solve the same name problem.

--- ソース.cpp
+++ ソース.cpp
@@ -13,7 +13,7 @@ R Semaphore(const std::function<R(void)>& F) {
 	return F();
 }/**/
 template<class R>
-R Semaphore(const std::function<R(void)>& F,std::mutex M) {
+R Semaphore2(const std::function<R(void)>& F,std::mutex M) {
 //	static std::mutex M;
 	std::lock_guard<std::mutex> L(M);
 	return F();
@@ -31,12 +31,13 @@ int main() {
 	/**/
 	for (std::size_t i = 0; i < 12800; i++) {
 		auto F = [i]()->bool {std::cout << i << std::endl; return true; };
-		//	Semaphore<bool>(F);
+	//	Semaphore<bool>(F);
+		auto FU = std::async(std::launch::async, Semaphore<bool>, F);//compiler cant solve to some of same name functions issue.
 		//	auto FU = std::async(std::launch::async, [F]() {return Semaphore(F); });//why not...
 	//	auto FU = std::async(std::launch::async, [](auto F) {Semaphore<bool>(F); 	std::this_thread::sleep_for(std::chrono::nanoseconds(3)); return true; }, F);
 		//	auto FU = std::async(std::launch::async, [i]() {std::cout << i << std::endl; });//why not...
 		//	auto FU = std::async(std::launch::async, [](auto i) {std::cout << i << std::endl; },i);//why not...
-		auto FU = std::async(std::launch::async, [](auto F,auto R) {Semaphore<bool>(F); 	std::this_thread::sleep_for(std::chrono::milliseconds(R)); return true; }, F,U(Rnd));
+	//	auto FU = std::async(std::launch::async, [](auto F,auto R) {Semaphore<bool>(F); 	std::this_thread::sleep_for(std::chrono::milliseconds(R)); return true; }, F,U(Rnd));
 	}
 	std::this_thread::sleep_for(std::chrono::seconds(3));
 	return 0;
